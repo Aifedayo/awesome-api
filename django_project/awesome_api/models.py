212a674e7a7e0229191fc45adecb6d6a1b5de605
akeemlag <akeemifedayolag@gmail.com>
2021-06-01 14:00:00 +0100

    awesome API with DRF

---
+++ django_project/awesome_api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Species(models.Model):
+    name = models.CharField(max_length=100)
+    classification = models.CharField(max_length=100)
+    language = models.CharField(max_length=100)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    birth_year = models.CharField(max_length=10)
+    eye_color = models.CharField(max_length=10)
+    species = models.ForeignKey(Species, on_delete=models.DO_NOTHING)
