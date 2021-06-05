bf316867a0ed59d4c5593af3c9be7417ce30090a
akeemlag <akeemifedayolag@gmail.com>
2021-06-05 14:00:00 +0100

    awesome API with DRF

--- django_project/awesome_api/views.py
+++ django_project/awesome_api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from awesome_api.serializers import PersonSerializer, SpeciesSerializers
+from awesome_api.models import Person, Species
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
