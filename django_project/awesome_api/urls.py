82094351a63b58283adb92f9c3a679121752294a
akeemlag <akeemifedayolag@gmail.com>
2021-06-08 14:00:00 +0100

    DRF router and urlpattern

--- django_project/awesome_api/urls.py
+++ django_project/awesome_api/urls.py
@@ -5,3 +5,9 @@ from rest_framework import router
 from awesome_api.views import PersonViewSet, SpeciesViewSet
 
 router = routers.DefaultRouter()
+router.register(r'people', PersonViewSet)
+router.register(r'species', SpeciesViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
