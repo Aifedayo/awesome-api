43541ce37aa170085284838aba1e6a89c7c36001
akeemlag <akeemifedayolag@gmail.com>
2021-06-14 14:00:00 +0100

    routers

--- django_project/awesome_api/models.py
+++ django_project/awesome_api/models.py
@@ -5,9 +5,14 @@ class Species(models.Model):
     classification = models.CharField(max_length=100)
     language = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
 class Person(models.Model):
     name = models.CharField(max_length=100)
     birth_year = models.CharField(max_length=10)
     eye_color = models.CharField(max_length=10)
     species = models.ForeignKey(Species, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
